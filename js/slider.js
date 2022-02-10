1151fbb9f9e07c575e712efc3445b99a528860eb
AntonBohn <anton.bohn@tudock.de>
2022-02-10 10:28:25 +0100

    edited the js to work with three slides

--- js/slider.js
+++ js/slider.js
@@ -1,86 +1,105 @@
 const CLASSNAMES = {
-    cur:'cur',
-    prev:'prev',
-    next:'next',
-    moving: 'moving'
+  cur: "cur",
+  prev: "prev",
+  next: "next",
+  moving: "moving",
 };
 
 const DIRECTIONS = {
-    back: -1,
-    fwd: 1
+  back: -1,
+  fwd: 1,
 };
 
-
-
 class Slider {
-    constructor(el) {
-        this.el = el;
-        this.initSlides();
-        this.direction = DIRECTIONS.fwd;
-    }
+  constructor(el) {
+    this.el = el;
+    this.initSlides();
+    this.direction = DIRECTIONS.fwd;
 
-    initSlides() {
-        this.slides = Array.from(document.querySelectorAll('.slider > *'));
-        this.cur = this.slides.findIndex(slide => slide.classList.contains(CLASSNAMES.cur));
-        this.prev = this.slides.findIndex(slide => slide.classList.contains(CLASSNAMES.prev));
-        this.next = this.slides.findIndex(slide => slide.classList.contains(CLASSNAMES.next));
-        this.max = this.slides.length - 1;
-        return this;
-    }
+    console.log(this);
+  }
 
-    fwd() {
-        if (this.moving) return;
-        this.direction = DIRECTIONS.fwd;
-        this.prepare();
-        this.cur = this.cur + 1 > this.max ? 0 : this.cur + 1;
-        this.next = this.cur === this.max ? 0 : this.cur + 1;
-        this.prev = this.cur === 0 ? this.max : this.cur - 1;
-        this.apply();
-    }
+  initSlides() {
+    this.slides = Array.from(document.querySelectorAll(".slider > *"));
+    this.itemCount = this.slides.length;
+    this.max = this.slides.length - 1;
+    this.cur = 0;
+    this.next = 1;
+    this.prev = this.max;
+    this.slides[this.cur].classList.add(CLASSNAMES.cur);
+    this.slides[this.next].classList.add(CLASSNAMES.next);
+    this.slides[this.prev].classList.add(CLASSNAMES.prev);
+    return this;
+  }
 
-    back() {
-        if (this.moving) return;
-        this.direction = DIRECTIONS.back;
-        this.prepare();
-        this.cur = this.cur - 1 < 0 ? this.max : this.cur - 1;
-        this.next = this.cur === 0 ? this.max : this.cur - 1;
-        this.prev = this.cur === this.max ? 0 : this.cur + 1;
-        this.apply();
-    }
+  fwd() {
+    if (this.moving) return;
+    this.direction = DIRECTIONS.fwd;
+    this.prepare();
+    this.cur = this.cur + 1 > this.max ? 0 : this.cur + 1;
+    this.next = this.cur === this.max ? 0 : this.cur + 1;
+    this.prev = this.cur === 0 ? this.max : this.cur - 1;
+    this.apply();
+  }
 
-    prepare() {
-        this.slides[this.cur].classList.remove(CLASSNAMES.cur);
-        this.slides[this.prev].classList.remove(CLASSNAMES.prev);
-        this.slides[this.next].classList.remove(CLASSNAMES.next);
-        return this;
-    }
+  back() {
+    if (this.moving) return;
+    this.direction = DIRECTIONS.back;
+    this.prepare();
+    this.cur = this.cur - 1 < 0 ? this.max : this.cur - 1;
+    this.next = this.cur === 0 ? this.max : this.cur - 1;
+    this.prev = this.cur === this.max ? 0 : this.cur + 1;
+    this.apply();
+  }
 
-    apply() {
-        this.slides[this.cur].addEventListener('transitionend', () => this.moving = false);
-        this.moving = true;
-        this.slides[this.cur].classList.add(CLASSNAMES.cur);
-        this.slides[this.prev].classList.add(CLASSNAMES.prev);
-        this.slides[this.next].classList.add(CLASSNAMES.next);
-        return this;
-    }
+  prepare() {
+    this.slides[this.cur].classList.remove(CLASSNAMES.cur);
+    this.slides[this.prev].classList.remove(CLASSNAMES.prev);
+    this.slides[this.next].classList.remove(CLASSNAMES.next);
+    return this;
+  }
 
-    set moving(m) {
-        this.el.classList.toggle(CLASSNAMES.moving, m === true);
-    }
+  apply() {
+    this.slides[this.cur].addEventListener(
+      "transitionend",
+      () => (this.moving = false)
+    );
+    this.moving = true;
+    this.slides[this.cur].classList.add(CLASSNAMES.cur);
+    this.slides[this.prev].classList.add(CLASSNAMES.prev);
+    this.slides[this.next].classList.add(CLASSNAMES.next);
+    return this;
+  }
 
-    get moving() {
-        return this.el.classList.contains(CLASSNAMES.moving);
-    }
+  set moving(m) {
+    this.el.classList.toggle(CLASSNAMES.moving, m === true);
+  }
 
-    set direction(d) {
-        this.el.style.setProperty('--move', d);
-    }
+  get moving() {
+    return this.el.classList.contains(CLASSNAMES.moving);
+  }
 
-    get direction() {
-        return this.el.style.getPropertyValue('--move');
-    }
+  set direction(d) {
+    this.el.style.setProperty("--move", d);
+  }
+
+  get direction() {
+    return this.el.style.getPropertyValue("--move");
+  }
+
+  set itemCount(i) {
+    this.el.style.setProperty("--item-count", i);
+  }
+
+  get itemCount() {
+    return this.el.style.getPropertyValue("--item-count");
+  }
 }
 
 const carousel = new Slider(document.querySelector(".slider"));
-document.querySelector('.buttons button:first-child').addEventListener('click', carousel.back.bind(carousel));
-document.querySelector('.buttons button:last-child').addEventListener('click', carousel.fwd.bind(carousel));
+document
+  .querySelector(".buttons button:first-child")
+  .addEventListener("click", carousel.back.bind(carousel));
+document
+  .querySelector(".buttons button:last-child")
+  .addEventListener("click", carousel.fwd.bind(carousel));
